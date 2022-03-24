1aece1ffccbd016ae034902ae72b4d8ab74e217f
Acrucha <mcaa@cin.ufpe.br>
2022-03-23 22:41:11 -0300

    Lista 2 - Questões D e I

--- L2/D_editorDeTexto.hs
+++ L2/D_editorDeTexto.hs
@@ -10,4 +10,32 @@ data Cmd = Cursor Int
          | Insert String
          deriving (Read)
 
+slice :: String -> Int -> Int -> String -> (Int, String)
+slice "Delete" cur n str = (cur, take cur str ++ drop (cur+n) str)
+slice "Backspace" cur n str | dif < 0 = (0, take n str)
+                            | otherwise = (dif, take dif str ++ drop cur str)
+       where dif = cur - n
+
+add :: String -> Int -> String -> (Int, String)
+add s cur str = (cur, take cur str ++ s ++ drop cur str)
+
+runCommand :: (Int, [Cmd]) -> String -> (Int, [Cmd], String)
+runCommand (cur, []) str = (cur, [], str)
+
+runCommand (cur, (Cursor n:as)) str | cur+n >= 0 = runCommand (cur+n, as) str
+                                    | otherwise = runCommand (0, as) str
+
+runCommand (cur, (Backspace n:as)) str = runCommand (fst sliceBack, as) (snd sliceBack)
+       where sliceBack = slice "Backspace" cur n str
+
+runCommand (cur, (Delete n:as)) str = runCommand (fst sliceFront, as) (snd sliceFront)
+       where sliceFront = slice "Delete" cur n str
+
+runCommand (cur, (Insert s:as)) str = runCommand (fst addString, as) (snd addString)
+       where addString = add s cur str
+
+
 editText :: String -> [Cmd] -> String
+editText str [] = str
+editText str cmds = result
+       where (f, s, result) = runCommand (0, cmds) str
--- L2/I_calculoSeguro.hs
+++ L2/I_calculoSeguro.hs
@@ -1,11 +1,27 @@
 import Prelude hiding (Maybe (..))
 
+main = do
+       a <- getLine
+       safeCalc a
+       
 data Maybe a = Just a |
                Nothing
                deriving(Show)
 
-main = do
-       a <- getLine
-       safeCalc a
+cutString :: String -> String -> (Int, Int, String)
+cutString n (a:b:c:as) | a >= 'a' && a <= 'z' = (read n :: Int, read as :: Int, [a,b,c])
+                       | otherwise = cutString (n ++ [a]) str
+       where str = (b:c:as)
+
+checkCommand :: (Int, Int, String) -> String
+checkCommand (a, b, "sum") = show (a + b)
+checkCommand (a, b, "mul") = show (a * b)
+checkCommand (a, b, "sub") | a < b = "(" ++ show (a - b) ++ ")"
+                           | otherwise = show (a - b)
+checkCommand (a, 0, "div") = "Nothing"
+checkCommand (a, b, "div") = show (a `div` b)
 
 safeCalc :: String -> IO ()
+safeCalc str | calc /= "Nothing" = putStr $ "Just " ++ calc
+             | otherwise = putStr $ calc
+       where calc = checkCommand (cutString [] str)
